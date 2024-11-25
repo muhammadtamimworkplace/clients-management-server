9794e0b6750d6d84eb68497cb9d196139755d304
Muhammad Tamim <muhammad.tamim.workplace@gmail.com>
2024-11-25 13:41:00 +0600

    mongodb user name & password make secure using .env file

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const dbPassword = process.env.BD_PASS;
-
 //Require dotenv
-// require('dotenv').config()
+require('dotenv').config()
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
